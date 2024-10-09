256c7b60afc9e809dee948f0fb4cbc969089ec27
Nick <nodhiambo01@gmail.com>
2024-10-09 12:35:37 +0300

    Initial commit

---
+++ authentication/urls.py
@@ -0,0 +1,16 @@
+# from django.urls import path
+# from .views import (
+#     SignUpView, CustomerSignUp, VendorSignUp,
+#     LoginView, LogoutView, VerifyEmailView,
+#     SwitchAccountView
+# )
+
+# urlpatterns = [
+#     path('api/v1', SignUpView, name='signup'),
+#     path('api/v1/customer/signup/', CustomerSignUp, name='customer'),
+#     path('api/v1/vendor/signup/', VendorSignUp, name='vendor'),
+#     path('api/v1/login/', LoginView, name='login'),
+#     path('api/v1/logout/', LogoutView, name='logout'),
+#     path('api/v1/verify-email/', VerifyEmailView, name='verify'),
+#     path('api/v1/switch-accounts/', SwitchAccountView, name='switch')
+# ]
